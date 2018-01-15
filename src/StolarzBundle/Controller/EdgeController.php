85c5c4832c010801aab67744f09cd94bce69321f
michal <michal@student.edu>
2018-01-15 20:55:16 +0100

    refaktoryzacja EdgeController

--- src/StolarzBundle/Controller/EdgeController.php
+++ src/StolarzBundle/Controller/EdgeController.php
@@ -2,21 +2,10 @@
 
 namespace StolarzBundle\Controller;
 
-//use StolarzBundle\Entity\Customer;
 use StolarzBundle\Entity\Edge;
-//use StolarzBundle\Entity\Material;
-//use StolarzBundle\Entity\Element;
-//use StolarzBundle\Entity\Order;
-//use StolarzBundle\Form\ElementType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-//use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-//use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-//use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-//use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-//use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 /**
  * @Route("/edge")
