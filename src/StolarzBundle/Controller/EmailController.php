ee95b4c26fcd8c27c0dedee97666b2d09ae61cb1
Michał Górniak <mgorniak@3a.pl>
2018-04-05 19:19:54 +0200

    Sending emails works

--- src/StolarzBundle/Controller/EmailController.php
+++ src/StolarzBundle/Controller/EmailController.php
@@ -13,15 +13,24 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class EmailController extends Controller
 {
     /**
-     * @Route("/", name="emailSend")
+     * @Route("/send/{orderId}", name="emailSend", requirements={"orderId": "\d+"})
      */
-    public function emailSendAction(Request $request)
+    public function emailSendAction($orderId, Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $order = $em->getRepository( 'StolarzBundle:Order' )->find( $orderId );
+        $customerId = $order->getCustomer()->getId();
+        $customer = $em->getRepository( 'StolarzBundle:Customer' )->find( $customerId );
 
+        $emailSubject = 'Zamówienie nr '. $orderId . '- Klient ' . $customer->getName() . '.';
+        $emailSender = array('michalek_18@wp.pl' => 'Michał');
+        $emailRecipient = 'michalgorniak@o2.pl';
+        $emailContent;
 
-        $message = (new \Swift_Message('Hello Email'))
-            ->setFrom('michalek_18@wp.pl')
-            ->setTo('michalgorniak@o2.pl')
+        $message = (new \Swift_Message())
+            ->setSubject($emailSubject)
+            ->setFrom($emailSender)
+            ->setTo($emailRecipient)
             ->setBody('Tresc przykladowego maila')
         ;
 
